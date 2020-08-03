3de7cede8161097e027f1de31a63c654d201482f
chrisBalher <c.baldeon.h@gmail.com>
2020-08-03 02:22:14 -0500

    Completado Sticky Header Functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu.js"
 import RevealOnScroll from "./modules/RevealOnScroll.js"
+import StickyHeader from "./modules/StickyHeader"
 
+let stickyheader = new StickyHeader
 let mobilemenu = new MobileMenu()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 65)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50)
