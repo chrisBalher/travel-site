f635db137c2292db3d6093df6febbafa74a8e600
chrisBalher <c.baldeon.h@gmail.com>
2020-08-03 23:26:19 -0500

    Actualizado cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,23 @@
 exports.handler = function(event, context, callback){
-    callback(null, {
-        statusCode:200,
-        body: "bienvenido al area secreta"
-    })
+    const secretContent = `
+    <h3>Bienvenido al Area Secreta</h3>
+    <p>Solo los papus pueden ingresar aquí</p>
+    `
+    let body 
+    if(event.body){
+        body = JSON.parse(event.body)
+    }else{
+        body = {}
+    }
+    if(body.password == "javascript"){
+        callback(null, {
+            statusCode:200,
+            body: secretContent
+        })
+    }else{
+        callback(null, {
+            statusCode:401
+        })
+    }
+    
 }
